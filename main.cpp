03ed743a7f4f61561ee0534af15441abbe0c1055
jackkrzyz <jackkrzyz@gmail.com>
2025-09-08 21:28:01 -0500

    replace big switch

--- main.cpp
+++ main.cpp
@@ -71,65 +71,29 @@ class Deck {
 };
 
 class Player {
-    public:
-        Card* hand[11];
-        int score = 0;
-        int cardsReceived = 0;
-        bool bust = false;
-        void addCard(Card* c) {
-            hand[cardsReceived] = c;
-            switch (c->value)
-            {
-                case 1:
-                    if (score + 11 <= 21) {
-                        score += 11;
-                    }
-                    else {
-                        score += 1;
-                    }
-                    break;
-                case 2:
-                    score += 2;
-                    break;
-                case 3:
-                    score += 3;
-                    break;
-                case 4:
-                    score += 4;
-                    break;
-                case 5:
-                    score += 5;
-                    break;
-                case 6:
-                    score += 6;
-                    break;
-                case 7:
-                    score += 7;
-                    break;
-                case 8:
-                    score += 8;
-                    break;
-                case 9:
-                    score += 9;
-                    break;                          
-                case 10:
-                    score += 10;
-                    break;
-                case 11:
-                    score += 10;
-                    break;
-                case 12:
-                    score += 10;
-                    break;
-                case 13:
-                    score += 10;
-                    break;
+public:
+    Card* hand[11];
+    int score = 0;
+    int cardsReceived = 0;
+    bool bust = false;
+    void addCard(Card* c) {
+        hand[cardsReceived] = c;
+        if (c->value == 1) {
+            if (score + 11 <= 21) {
+                score += 11;
+            } else {
+                score += 1;
             }
-            if (score > 21) {
-                bust = true;
-            }
-            cardsReceived++;
+        } else if (c->value >= 10) {
+            score += 10;
+        } else {
+            score += c->value;
         }
+        if (score > 21) {
+            bust = true;
+        }
+        cardsReceived++;
+    }
 };
 
 
